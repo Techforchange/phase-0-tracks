5b59a655348fe0b83b3a70a146316eb2690611c0
Techforchange <arleneperezsb@gmail.com>
2016-10-01 18:58:26 -0700

    Created wordguess file

---
+++ ruby/6.6_game/Wordguess.rb
@@ -0,0 +1,12 @@
+#create a class named word_guess
+
+
+
+
+#driver code details: user focused; There are 2 users
+#input: user1 gives word 
+#input: user 2 guesses word of user1
+#guesses are limited but repeated guesses are allowed
+#The number of guesses = length of the word
+# return: string such as "_ _ _" or "_ a _" if user2 guesses "a" and the word is "cat"
+#at the end: print a congratulatory message if they win or taunting message if they lose
