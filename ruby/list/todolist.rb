a7f97f23272d21d052c7d8035c55ce914510795c
Techforchange <arleneperezsb@gmail.com>
2016-10-01 17:45:34 -0700

    update

--- ruby/list/todolist.rb
+++ ruby/list/todolist.rb
@@ -1,3 +1,24 @@
-class Todolist
-	p "hello"
-end
+class TodoList
+	
+	def initialize(list)
+		@list = list
+	end
+
+
+	def get_items
+		p @list
+	end
+
+	
+  	def add_item(item)
+  		@list.push item
+  	end
+
+  	def delete_item(item)
+  		@list.delete item
+  	end
+
+  	def get_item(int)
+  		@list[int]
+  	end
+ end
