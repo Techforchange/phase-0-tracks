723e067b14f3d2085e3b49f6902b53c913781f14
Techforchange <arleneperezsb@gmail.com>
2016-10-03 09:09:34 -0700

    added is over test

--- ruby/6.6_game/Wordguess_spec.rb
+++ ruby/6.6_game/Wordguess_spec.rb
@@ -4,7 +4,10 @@ describe Wordguess do
 	let(:game) {Wordguess.new(word,guess)}
 
 	it "checks if User 2's guess is in User 1's word"
-		expect(check_word).to eq ("h_ _ _ _")
+		expect(game.check_word).to eq ("h_ _ _ _")
 	end
 
+	it "checks if player guessed the whole word"
+		expect(game.is_over). to eq ("Yes!")
+	end
 end
