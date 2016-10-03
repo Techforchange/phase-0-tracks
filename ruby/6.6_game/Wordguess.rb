4fae854b9439f0aecd752b880d3e786dd22b1c5e
Techforchange <arleneperezsb@gmail.com>
2016-10-03 09:09:11 -0700

    updated with loop

--- ruby/6.6_game/Wordguess.rb
+++ ruby/6.6_game/Wordguess.rb
@@ -1,16 +1,17 @@
 #create a class named word_guess
 class Wordguess
-	attr_reader :guess_count, :is_over, :word, :guess
+	attr_reader :guess_count, :is_over, :word, :guess, :arr
 
 #attributes: guess_count, is_over, word
 	def initialize(word,guess)
 		@word = word.split(//)
 		@guess = guess.to_str
-		@guess_count = 0
+		@guess_count = word.length
 		@is_over = false
+		@arr = arr
 	end
 
-	def check_word
+	def check_word(guess)
 		arr = []
 		arr.length == word.length
 		arr = word.map do |letter|
@@ -23,35 +24,41 @@ class Wordguess
 		p arr.join
 		
 	end
+	
+	def is_over
+		if arr == word
+			p "You won!!! Congrats!"
+		else
+			p "Really? It wasn't THAT hard. You should be ashamed lol Joking!"
+		end
+	end
+			
 end
 
 
-#guesses are limited but repeated guesses are allowed
-#The number of guesses = length of the word
-# return: string such as "_ _ _" or "_ a _" if user2 guesses "a" and the word is "cat"
-#at the end: print a congratulatory message if they win or taunting message if they lose
 
 
 #driver code details: user focused; There are 2 users
 #input: user1 gives word 
 #input: user 2 guesses word of user1
 
-puts "Welcome to the Guess Word Game! There will be two Users for this game. User 1, you're up first!"
-
-
-
-puts "User 1: Type in your word"
+puts "Welcome to the Guess Word Game! There will be two Users for this game. User 1, you're up first! Type in your 'secret' word:"
 word = gets.chomp.to_str
-
-puts "User 2, you're up! Guess User 1's word by typing in one letter."
-guess = gets.chomp.to_str
-
-game = Wordguess.new(word,guess)
-game.check_word()
 
 # p "#{game.user1_word}"
 
 # game.check_word(user1_word)
+guess_count = 0
+while guess_count < word.length
+		puts "User 2, you're up! Guess User 1's word by typing in one letter."
+		guess = gets.chomp.to_str
+		game = Wordguess.new(word,guess)
+		game.check_word(guess)
+		guess_count += 1
+end
+game.is_over
+
+	
 
 
 
@@ -60,11 +67,8 @@ game.check_word()
 
 
 
-
-
-
-
-
+	
+	
 
 
 
