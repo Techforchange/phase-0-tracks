d16cd235bac3ab664dc52bf9d4e77ab377e5c103
Techforchange <arleneperezsb@gmail.com>
2016-10-04 20:12:30 -0700

    Created object structure to log name and color for horse

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,4 +1,12 @@
 //Declare an array of four colors 
-var colors = ["pink","purple","red","white"];
+var color = ["pink","purple","red","white"];
 //Declare an array of four names
 var names = ["Bob","Mike","David","Coco"];
+//Add a code that adds another color to the colors array
+color.push("black");
+//Add a code that adds another name to the names array
+names.push("George");
+// The keys of your object should be horse names, and the values should be colors.
+for (var i = color.length; i > 0; i--)
+var horse = { names[str]:colors[str]};
+console.log("This horse's name is" + horse['name'] + "and its color is" + color['color']);
