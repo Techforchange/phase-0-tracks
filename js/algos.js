2a92a35fdb9a1531885f7ef83102898d8f5a36d8
Techforchange <arleneperezsb@gmail.com>
2016-10-06 00:26:53 -0700

    added driver code to test the function

--- js/algos.js
+++ js/algos.js
@@ -1,6 +1,8 @@
 //write a function that takes an array of words or phrases and returns the longest word or phrase in the array.
-var names = ["arlene", "martin", "angelica", "christina"];
-//every element in the string get its length
+//You need to create an empty string to store the "longest word" and have something to compare each index length to
+//For every element in the string, you need its length
+//Compare the first element with the empty string to overwrite it
+//Continue comparing each additional element with the stored string
 function longestword(array){
 	var longest = "";
 
@@ -11,17 +13,16 @@ function longestword(array){
 		else if (array[i].length < longest.length){
 		 longest = longest;
 		}
-		else (array[i].length = longest.length);{
-			longest = longest + array[i];
-		}
+		//else (array[i].length = longest.length);{
+			//longest = longest + array[i];
+		//}
 	}
 	console.log(longest);
 }
-
-//var x = ["ha", "lo", "lol"];
-console.log(longestword("ha", "lo", "lol"));
-//Get each element within the string by its index using a conditional statement(similar to each method in ruby)
-//Set a variable equal to that element
-//Find the length of each string by x.length
-//input: array output: longest element in the array
 //Driver code that tests a few arrays
+
+longestword(["have", "lo", "lol"]);
+longestword(["have", "lolololo", "lolcacwecqvfref3"]);
+longestword(["holdde3df231", "lo", "lol"]);
+longestword(["have", "lo", "hallelujah","lol", "kikss"]);
+
