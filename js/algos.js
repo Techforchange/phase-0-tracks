13641c6177d88625b3b5de94fbfac7361daa269f
Techforchange <arleneperezsb@gmail.com>
2016-10-05 18:30:40 -0700

    Found the length of each string in the array

---
+++ js/algos.js
@@ -0,0 +1,18 @@
+//write a function that takes an array of words or phrases and returns the longest word or phrase in the array.
+var names = ["arlene", "martin", "angelica", "christina"];
+//every element in the string get its length
+function longestword(array){
+
+	for (var i = 0; i < names.length; i++){
+		console.log(names[i].length);
+	}
+
+
+}
+
+longestword(names);
+//Get each element within the string by its index using a conditional statement(similar to each method in ruby)
+//Set a variable equal to that element
+//Find the length of each string by x.length
+//input: array output: longest element in the array
+//Driver code that tests a few arrays
