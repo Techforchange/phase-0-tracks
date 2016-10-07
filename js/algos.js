ab4f0826c3c099dafd50673fc07d50afad380d96
Techforchange <arleneperezsb@gmail.com>
2016-10-07 02:14:30 -0700

    added release 1

--- js/algos.js
+++ js/algos.js
@@ -3,8 +3,8 @@
 //For every element in the string, you need its length
 //Compare the first element with the empty string to overwrite it
 //Continue comparing each additional element with the stored string
-function longestword(array){
-	var longest = "";
+/*function longestword(array){
+	var longest = [];
 
 	for (var i = 0; i < array.length; i++){
 		if (array[i].length > longest.length) {
@@ -19,16 +19,60 @@ function longestword(array){
 	}
 	console.log(longest);
 }
+*/	
+//write a function that takes two objects 
+
+function keymatch (array){
+
+
+  if (array[0].key == array[1].key){
+			console.log("true");
+	}
+	else if (array[0].value == array[1].value) {
+		console.log("true");
+	}
+	else {
+		console.log("false");
+	} 
+}
+
+function RandomTest(int){
+arr = [];
+word = 
+word.length = Math.floor(Math.random() * 10) + 1;  
+//word.length = 5
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var RandomLetters = [];
+for (var i = 0; i < word.length; i ++;){
+	RandomLetters.push(alphabet(Math.floor(Math.random() * 26) + 1));
+	return RandomLetters.join();
+}
+
+for (var i = 0; i < int; i ++;){
+	arr.push(word);
+}
+
+//var example = [{name: "Steven", age: 6},{name: "Mark", age: 54}];
+
+//keymatch(example);
+
+
+//array[x] = y == array2[x] = y
+//Create an empty array to push the two hashes
+//If the two objects share at least one value => true	
+//else false
+//Call each index of the array, in this case each object by its key and value
+
+
+
+//Checks to see if the objects share at least one key-value pair. 
+
 //Driver code that tests a few arrays
 
-longestword(["have", "lo", "lol"]);
+/*longestword(["have", "lo", "lol"]);
 longestword(["have", "lolololo", "lolcacwecqvfref3"]);
 longestword(["holdde3df231", "lo", "lol"]);
 longestword(["have", "lo", "hallelujah","lol", "kikss"]);
+*/
 
-//write a function that takes two objects 
-
-function keymatch (name, age){
-	
-}
-//Checks to see if the objects share at least one key-value pair. 
