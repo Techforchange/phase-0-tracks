8c3b0a31dfe8b16bfee25b8efb1c372f4a08c2bb
Techforchange <arleneperezsb@gmail.com>
2016-10-17 07:10:21 -0700

    Created table

--- databases/8.5_project/8.5_project.rb
+++ databases/8.5_project/8.5_project.rb
@@ -4,3 +4,13 @@ require 'sqlite3'
 
 #create SQlite3 database
 db = SQlite3::Database.new("_____.db")
+#To view input:
+db.execute("SELECT * FROM kittens")
+
+#delimiters
+create_table = <<-SQL
+	CREATE TABLE ______ (
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(255),
+	age INT)
+SQL
